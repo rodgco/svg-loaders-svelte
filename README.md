15482dbd29d257ea258704234a4bc89b86356107
Rodrigo Couto <r@rodg.co>
2021-06-18 17:37:49 -0300

    Initial Commit

---
+++ README.md
@@ -0,0 +1,89 @@
+This is a zero-dependency Svelte adaptation of Sam Herberts [SVG Loaders](https://github.com/SamHerbert/SVG-Loaders) library. Highly influenced by ajwann [SVG Loaders React](https://github.com/ajwann/svg-loaders-react) adaptation.
+
+# Usage
+
+## Install from NPM
+
+```bash
+npm install svg-loaders-svelte
+```
+
+## Import the SVGLoaders components
+
+### Import all the loaders in a namespaced fashion
+
+You can import all the loaders at once:
+
+```js
+import * as SVGLoaders from "svg-loaders-svelte";
+```
+
+and use them in a namespaced manner:
+
+```js
+<SVGLoaders.Bars />
+```
+
+### Import an individual loader
+
+You can also import a single loader:
+
+```js
+import { Bars } from "svg-loaders-svelte";
+```
+
+and use it without any fancy namespacing:
+
+```js
+<Bars />
+```
+
+## Components
+
+`<Audio />`
+
+`<BallTriangle />`
+
+`<Bars />`
+
+`<Circles />`
+
+`<Grid />`
+
+`<Hearts />`
+
+`<Oval />`
+
+`<Puff />`
+
+`<Rings />`
+
+`<SpinningCircles />`
+
+`<TailSpin />`
+
+`<ThreeDots />`
+
+# Options
+
+Each of these components should be able to accept any [SVG tag presentation attributes](https://developer.mozilla.org/en-US/docs/Web/SVG/Attribute/Presentation) as props.
+
+Loaders SVGs will be inlined which enabled the use of `fill="currentColor"`, and makes it easier to style the loader from CSS.
+
+# Common Usage
+
+```Javascript
+// render the Audio loader with Svelte orange color through CSS
+<span style="color: #ff3e00">
+  <Audio />
+</span>
+
+// render the Puff loader with a stroke opacity of .125
+<Puff strokeOpacity=".125" />
+
+// render the Puff loader with a stroke of mint green
+<Puff stroke="#98ff98" />
+
+// render the Puff loader with a stroke of mint green and a stroke opactiy of .125
+<Puff stroke="#98ff98" strokeOpacity=".125"/>
+```
---
+++ src/routes/index.svelte
@@ -0,0 +1,136 @@
+<script>
+  import Audio from "$lib/Audio.svelte";
+  import BallTriangle from "$lib/BallTriangle.svelte";
+  import Bars from "$lib/Bars.svelte";
+  import Circles from "$lib/Circles.svelte";
+  import Grid from "$lib/Grid.svelte";
+  import Hearts from "$lib/Hearts.svelte";
+  import Oval from "$lib/Oval.svelte";
+  import Puff from "$lib/Puff.svelte";
+  import Rings from "$lib/Rings.svelte";
+  import SpinningCircles from "$lib/SpinningCircles.svelte";
+  import TailSpin from "$lib/TailSpin.svelte";
+  import ThreeDots from "$lib/ThreeDots.svelte";
+</script>
+
+<svelte:head>
+  <title>SVG Loaders Svelte - Rodrigo Couto</title>
+</svelte:head>
+
+<ul>
+  <li>
+    <Audio width="40" height="58" alt="" />
+  </li>
+  <li>
+    <Rings width="60" height="60" alt="" />
+  </li>
+  <li>
+    <Grid width="40" height="40" alt="" />
+  </li>
+  <li>
+    <Hearts width="80" height="36" alt="" />
+  </li>
+  <li>
+    <Oval width="50" height="50" alt="" />
+  </li>
+  <li>
+    <ThreeDots width="60" height="60" alt="" />
+  </li>
+  <li>
+    <SpinningCircles width="50" height="50" alt="" />
+  </li>
+  <li>
+    <Puff width="50" height="50" alt="" />
+  </li>
+  <li>
+    <Circles width="50" hight="50" alt="" />
+  </li>
+  <li>
+    <TailSpin width="50" height="50" alt="" />
+  </li>
+  <li>
+    <Bars width="40" height="40" alt="" />
+  </li>
+  <li>
+    <BallTriangle width="50" height="50" alt="" />
+  </li>
+  <div style="clear: both" />
+</ul>
+
+<div style="clear: both" />
+
+<div class="attr">
+  Made by <a href="https://twitter.com/sherb">Sam</a>. For everyone. Adapted by
+  <a href="https://twitter.com/rodg_co">Rodgco</a>, for the <a href="http://svelte.dev">Svelte</a> Community
+</div>
+
+<style>
+  ul {
+    color: #fff;
+    background: #fff0;
+    max-width: 720px;
+    margin: auto;
+    margin-top: 60px;
+    list-style: none;
+    padding: 0;
+    box-shadow: 0px 0px 4px 0px rgba(0, 0, 0, 0.37);
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+  }
+  ul li {
+    min-width: 180px;
+    min-height: 180px;
+    aspect-ratio: 1/1;
+    line-height: 176px;
+    flex-grow: 1;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background-repeat: no-repeat;
+    background-position: center;
+  }
+  li:nth-child(1) {
+    background-color: #1abc9c;
+  }
+  li:nth-child(2) {
+    background-color: #34495e;
+  }
+  li:nth-child(3) {
+    background-color: #f39c12;
+  }
+  li:nth-child(4) {
+    background-color: #9b59b6;
+  }
+  li:nth-child(5) {
+    background-color: #3498db;
+  }
+  li:nth-child(6) {
+    background-color: #e74c3c;
+  }
+  li:nth-child(7) {
+    background-color: #2ecc71;
+  }
+  li:nth-child(8) {
+    background-color: #2c3e50;
+  }
+  li:nth-child(9) {
+    background-color: #f1c40f;
+  }
+  li:nth-child(10) {
+    background-color: #8e44ad;
+  }
+  li:nth-child(11) {
+    background-color: #2980b9;
+  }
+  li:nth-child(12) {
+    background-color: #e74c3c;
+  }
+
+  .attr {
+    color: #fff;
+    text-align: center;
+    font-size: 0.875rem;
+    margin-top: 20px;
+  }
+</style>
