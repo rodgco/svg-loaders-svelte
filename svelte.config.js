f05b8441b142337e2320ce773cbaf8deb9521157
Rodrigo Couto <r@rodg.co>
2022-08-14 22:56:14 -0300

    move to pnpm & bump deps + config files

--- svelte.config.js
+++ svelte.config.js
@@ -1,18 +1,15 @@
-import adapter from "@sveltejs/adapter-static";
+import adapter from '@sveltejs/adapter-auto';
+import preprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-  kit: {
-    // hydrate the <div id="svelte"> element in src/app.html
-    target: "#svelte",
-    adapter: adapter({
-      out: "build/path"
-    }),
-    paths: {
-      base: "/svg-loaders-svelte",
-      assets: "/svg-loaders-svelte"
-    }
-  }
+	// Consult https://github.com/sveltejs/svelte-preprocess
+	// for more information about preprocessors
+	preprocess: preprocess(),
+
+	kit: {
+		adapter: adapter()
+	}
 };
 
 export default config;
